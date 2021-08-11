3687b3811cacd9c78803405a1e1f417867dd0460
Daria_Zaika <dashazaika@gmail.com>
2021-08-11 21:09:56 +0300

    hw28 rebased

--- homeworks/CarDealer/apps/orders/views.py
+++ homeworks/CarDealer/apps/orders/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.core import serializers
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from apps.orders.models import Order
+from django.shortcuts import get_object_or_404, reverse
 
-# Create your views here.
+
+class OrderView(ListView):
+    model = Order
+    template_name = 'order_list.html'
+    context_object_name = 'all_orders_list'
+
+    def get_queryset(self):
+        return Order.objects.all()
+
+
+class OrderDetailView(DetailView):
+    model = Order
+    template_name = 'order_detail.html'
+
+
+def serializer_order(request):
+    data = serializers.serialize('json', Order.objects.all())
+    return HttpResponse(request.method + '<br>' + data)
